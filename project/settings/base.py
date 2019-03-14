3e1c6f7df0445e1a2fdb6edff5d27d1794572732
Anatoly <a.feteleu@spider.ru>
2019-03-14 15:02:07 +0300

    Fixed paths for media & static dirs

--- project/settings/base.py
+++ project/settings/base.py
@@ -187,15 +187,15 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 # Celery settings
