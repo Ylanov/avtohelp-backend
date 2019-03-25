27cec443353759b08cdb89272e610b795f2c6d3a
Anatoly <a.feteleu@spider.ru>
2019-03-25 16:20:40 +0300

    Fixed settings

--- project/settings/base.py
+++ project/settings/base.py
@@ -187,11 +187,11 @@ LOCALE_PATHS = (
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PUBLIC_ROOT, 'static'),
 )
 
 
