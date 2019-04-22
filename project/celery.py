32f72271abad2a9ae27005c475265b96f0460ecb
Anatoly <a.feteleu@spider.ru>
2019-04-22 14:28:27 +0300

    Comment unused methods

--- project/celery.py
+++ project/celery.py
@@ -36,9 +36,10 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(minute=settings.REQUEST_RELEVANCE),
                              check_request_relevance.s(),
                              name='Check assistance request relevance')
-    sender.add_periodic_task(crontab(minute=settings.MESSAGES_UPDATE_PERIOD),
-                             notify_unread_messages.s(),
-                             name='Notify users about unread messages')
+    # Unused
+    # sender.add_periodic_task(crontab(minute=settings.MESSAGES_UPDATE_PERIOD),
+    #                          notify_unread_messages.s(),
+    #                          name='Notify users about unread messages')
 
 
 @app.task
@@ -55,40 +56,41 @@ def check_request_relevance():
                 request.save()
 
 
-@app.task
-def notify_unread_messages():
-    """Notify users about unread messages"""
-    from base import models as base_models
-    from chat import models as chat_models
-    from fcm_django.models import FCMDevice
-
-    rooms = chat_models.ChatRoom.objects.all()
-    for room in rooms:
-        notify = list()
-        for participant in room.participants.all():
-            message_count = room.chatmessage_set.exclude(sender=participant).count()
-            read_messages = chat_models.ChatReadMessage.objects.filter(user=participant).count()
-            if (message_count - read_messages) > settings.LIMIT_UNREAD_MESSAGES:
-                notify.append(participant)
-            # for message in room.chatmessage_set.all():
-            #     qs = chat_models.ChatReadMessage.objects.filter(message=message, user=participant)
-            #     if not qs.exists():
-            #         notify.append(participant)
-        if notify:
-            for user in notify:
-                notification = base_models.PushNotification.objects.create(
-                    user=user,
-                    title=_('Unread messages'),
-                    description=_('You have unread messages')
-                )
-                devices = FCMDevice.objects.filter(user=user)
-                if devices.exists():
-                    count = devices.send_message(**notification.get_push_dict())
-                    if count.get('success') > 0:
-                        notification.status = True
-                        notification.save()
-                        logger.info(f'Users notified: {count.get("success")}')
-                    else:
-                        logger.info(
-                            f'Error was occurred when sending PUSH-notifications. Failed: {count.get("failure")}')
-        notify.clear()
+# Unused
+# @app.task
+# def notify_unread_messages():
+#     """Notify users about unread messages"""
+#     from base import models as base_models
+#     from chat import models as chat_models
+#     from fcm_django.models import FCMDevice
+#
+#     rooms = chat_models.ChatRoom.objects.all()
+#     for room in rooms:
+#         notify = list()
+#         for participant in room.participants.all():
+#             message_count = room.chatmessage_set.exclude(sender=participant).count()
+#             read_messages = chat_models.ChatReadMessage.objects.filter(user=participant).count()
+#             if (message_count - read_messages) > settings.LIMIT_UNREAD_MESSAGES:
+#                 notify.append(participant)
+#             # for message in room.chatmessage_set.all():
+#             #     qs = chat_models.ChatReadMessage.objects.filter(message=message, user=participant)
+#             #     if not qs.exists():
+#             #         notify.append(participant)
+#         if notify:
+#             for user in notify:
+#                 notification = base_models.PushNotification.objects.create(
+#                     user=user,
+#                     title=_('Unread messages'),
+#                     description=_('You have unread messages')
+#                 )
+#                 devices = FCMDevice.objects.filter(user=user)
+#                 if devices.exists():
+#                     count = devices.send_message(**notification.get_push_dict())
+#                     if count.get('success') > 0:
+#                         notification.status = True
+#                         notification.save()
+#                         logger.info(f'Users notified: {count.get("success")}')
+#                     else:
+#                         logger.info(
+#                             f'Error was occurred when sending PUSH-notifications. Failed: {count.get("failure")}')
+#         notify.clear()
