06cf020ba8ce345b6c41a469fafd862f3b9871a1
Anatoly <a.feteleu@spider.ru>
2019-06-10 17:51:12 +0300

    Added parameter "room_id" to PUSH-notification body

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -94,11 +94,13 @@ class ChatMessage(BaseMixin):
         if settings.USE_CELERY:
             tasks.notify_chat_participants.delay(
                 sender_id=self.sender.id,
+                room_id=self.room.id,
                 participants=list(offline_users)
             )
         else:
             tasks.notify_chat_participants(
                 sender_id=self.sender.id,
+                room_id=self.room.id,
                 participants=list(offline_users)
             )
 
--- project/celery.py
+++ project/celery.py
@@ -138,7 +138,7 @@ def notify_friend_request(invited_id):
 
 
 @app.task
-def notify_chat_participants(sender_id, participants):
+def notify_chat_participants(sender_id, room_id, participants):
     """Notify user about new friend request"""
     from account import models as account_models
     from base import models as base_models
@@ -154,7 +154,7 @@ def notify_chat_participants(sender_id, participants):
         )
         devices = FCMDevice.objects.filter(user_id=user_id)
         if devices.exists():
-            raw_result = devices.send_message(**notification.get_push_dict())
+            raw_result = devices.send_message(**notification.get_push_dict(room_id=room_id))
             result = raw_result if hasattr(raw_result, 'get') else {k: v for k, v in raw_result[0].items()}
             if result.get('success') > 0:
                 notification.status = True
