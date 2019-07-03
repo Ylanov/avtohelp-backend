8edc6f40fcfe567960637f50d113fdc2d14e05b3
Anatoly <a.feteleu@spider.ru>
2019-07-03 12:42:11 +0300

    Disable static verificatoin SMS-code

--- apps/authorization/serializers/current.py
+++ apps/authorization/serializers/current.py
@@ -1,3 +1,6 @@
+# todo: delete after testing
+from os import environ
+
 from django.conf import settings
 from django.utils import timezone
 from phonenumber_field.serializerfields import PhoneNumberField
@@ -47,7 +50,10 @@ class PhoneVerificationSerializer(serializers.ModelSerializer):
         # make a new user
         user = User.objects.get_or_make(phone=validated_data.get('phone'))[0]
         # make a new sms
-        obj = models.SMSCode.objects.make(user=user, code=12345, **validated_data)
+        if environ.get('SETTINGS_CONFIGURATION') in ('local', 'development'):
+            obj = models.SMSCode.objects.make(user=user, code=12345, **validated_data)
+        else:
+            obj = models.SMSCode.objects.make(user=user, **validated_data)
         if settings.USE_CELERY:
             tasks.send_verification_sms.delay(sms_code_id=obj.id)
         else:
