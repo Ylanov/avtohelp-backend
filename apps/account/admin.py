ed83720ecb99624f6682158ee912e4879f6bbd2f
Anatoly <a.feteleu@spider.ru>
2019-07-24 14:01:18 +0300

    Added search field to account admin panel and refactored a little bit

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
 
@@ -6,7 +8,13 @@ class UserAdminModel(admin.ModelAdmin):
     """Custom admin page for User"""
 
     readonly_fields = ('id', 'profile', 'phone', 'created', 'modified')
+    search_fields = ('phone', )
     list_display = ('id', 'profile', 'phone', 'created', 'modified')
+    fieldsets = (
+        (_('User\'s data'), {'fields': ('id', 'phone')}),
+        (_('Info'), {'fields': ('created', 'modified')}),
+        (_('Flags'), {'fields': ('is_active', 'is_staff')})
+    )
 
 
 # Register your models here.
