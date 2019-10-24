e526d11a024035bfa6e33f4932bdbf3ab1ede5c2
Maxim Klyachev <maxim@maxber.ru>
2019-10-24 18:18:19 +0300

    [rh-27][task] add filter to chat rooms list

--- apps/chat/views/current.py
+++ apps/chat/views/current.py
@@ -71,7 +71,8 @@ class ChatRoomListView(generics.ListAPIView):
         return models.ChatRoom.objects.by_participant(participant=user)\
                                       .annotate_last_message_datetime()\
                                       .annotate_unread_messages(user)\
-                                      .annotate_message_count()
+                                      .annotate_message_count()\
+                                      .filter(message_count__gte=1)
 
 
 class ChatView(generics.GenericAPIView):
