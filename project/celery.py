6e07303c2ff42b4cdfe70de1b8fe700b930f0c0b
Maxim Klyachev <maxim@maxber.ru>
2019-12-29 17:22:22 +0300

    [hotfix] set phone number for market approve

--- project/celery.py
+++ project/celery.py
@@ -78,7 +78,7 @@ def send_verification_sms(sms_code_id):
     # Get sms code object
     sms = auth_models.SMSCode.objects.get(id=sms_code_id)
 
-    if settings.USE_SMS is True:
+    if settings.USE_SMS is True and sms.user.phone != '+79189383399':
         # send actual sms if its allowed by server configuration
         try:
             # get config
